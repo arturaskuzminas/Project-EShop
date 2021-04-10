3c82d922a53f72124a59051bc6b1d04bb4faa11f
ArturasK <arturaskuzminass@gmail.com>
2021-04-10 19:04:19 +0300

    Added navbar

--- Laikrodziai/wwwroot/js/site.js
+++ Laikrodziai/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
