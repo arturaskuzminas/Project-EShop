7fac9b315ed797e88bf353bb91ddf450f166ea59
arturas <arturaskuzminass@gmail.com>
2021-04-24 19:03:04 +0300

    added project

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyShop.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string CityID { get; set; }
+
+        [ForeignKey("CityID")]
+        public CityModel City { get; set; }
+    }
+}
---
+++ Models/CityModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyShop.Models
+{
+    public class CityModel
+    {
+        [Key]
+        [Required(ErrorMessage = "Laukas 'Miesto kodas' yra privalomas")]
+        [Display(Name = "Miesto kodas")]
+        public string ID { get; set; }
+
+        [Required(ErrorMessage = "Laukas 'Miesto pavadinimas' yra privalomas")]
+        [Display(Name = "Miesto pavadinimas")]
+        public string Name { get; set; }
+    }
+}
