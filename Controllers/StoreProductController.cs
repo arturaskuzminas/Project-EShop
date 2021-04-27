c6190f2a4bcd27b5fd22a01bd3aed290a20ebf8f
arturas <arturaskuzminass@gmail.com>
2021-04-27 21:11:46 +0300

    added search functionality

--- Controllers/StoreProductController.cs
+++ Controllers/StoreProductController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyShop.Data;
+using MyShop.Models;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyShop.Controllers
@@ -33,9 +36,20 @@ namespace MyShop.Controllers
         }
 
         // GET: StoreProducts
-        public async Task<IActionResult> Products()
+        [HttpGet]
+        public async Task<IActionResult> ProductsAsync(string id)
         {
-            return View(await _context.Products.ToListAsync());
+            IEnumerable<ProductModel> results = await _context.Products.ToListAsync();
+
+            if (string.IsNullOrEmpty(id) || id.Length < 3)
+            {
+                ViewBag.searchStr = "incorrect";
+                return View();
+            }
+            else {
+                results = results.Where(s => s.Title.StartsWith(id));
+                return View(results);
+            }
         }
     }
 }
