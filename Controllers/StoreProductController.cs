89d4d1983f1d652c54a33fc4d0658351657da36b
arturas <arturaskuzminass@gmail.com>
2021-04-27 18:29:43 +0300

    added links to products in database

---
+++ Controllers/StoreProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyShop.Data;
+using MyShop.Models;
+
+namespace MyShop.Controllers
+{
+    public class StoreProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StoreProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: StoreProduct
+        public async Task<IActionResult> Product(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productModel = await _context.Products.FindAsync(id);
+            if (productModel == null)
+            {
+                return NotFound();
+            }
+            return View(productModel);
+        }
+    }
+}
